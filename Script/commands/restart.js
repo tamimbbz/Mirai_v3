25693cf7dc409da544bf43023f0a2e68a8aee451
rxabdullah0007 <rxabdullah0007@gmail.com>
2025-12-16 19:32:38 +0600

    Upload Script/commands/restart.js

---
+++ Script/commands/restart.js
@@ -0,0 +1,15 @@
+module.exports.config = {
+	name: "restart",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "𝐫𝐗",
+	description: "Restart Bot",
+	commandCategory: "system",
+	usages: "",
+	cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+	const { threadID, messageID } = event;
+	return api.sendMessage(` ⥤↻⥢ ${global.config.BOTNAME} 𝐢𝐬 𝐧𝐨𝐰 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐢𝐧𝐠...`, threadID, () => process.exit(1));
+}
