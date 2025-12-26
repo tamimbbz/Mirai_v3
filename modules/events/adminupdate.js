3cec9e2f3d2703a96285011804780a27de240d26
tamimbbz <tanishajannat721615@gmail.com>
2025-12-26 12:36:04 +0600

    Bbz

--- modules/events/adminupdate.js
+++ modules/events/adminupdate.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adminUpdate",
 	eventType: ["log:thread-admins","log:thread-name", "log:user-nickname","log:thread-icon","log:thread-call","log:thread-color"],
 	version: "1.0.1",
-	credits: "rX",
+	credits: "bbz",
 	description: "Update team information quickly",
     envConfig: {
         sendNoti: true,
